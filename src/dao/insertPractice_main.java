5ccb620e031fac38ed69be0dc1f2dd00d7c5e650
cheolho <gghg0000@gmail.com>
2019-01-28 00:07:10 +0900

    2019-01-27 HOME

---
+++ src/dao/insertPractice_main.java
@@ -0,0 +1,21 @@
+package dao;
+
+public class insertPractice_main {
+
+    //
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        Project_DAO dao = new Project_DAO(controller);
+        AutoGenerator_employee ag = new AutoGenerator_employee();
+        int startNum = 1000;
+        int lastNum = 2000;
+        int volume = lastNum - startNum + 1;
+        long st = System.currentTimeMillis();
+        ag.insertAllEmps(ag.employeeGenerator(dao, startNum, lastNum), dao);
+        long et = System.currentTimeMillis();
+        int result = (int)(et-st)/1000;
+        System.out.println(volume+"회 실행에 걸린 시간 : "+result);
+
+    }
+}
